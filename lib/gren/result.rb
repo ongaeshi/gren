b48f94b806308d2a01f149fb76cd5fde9c34c67f
ongaeshi <ongaeshi@example.com>
2010-07-25 03:10:32 +0900

    テキスト整形を修正

--- lib/gren/result.rb
+++ lib/gren/result.rb
@@ -25,22 +25,39 @@ module Gren
       e.utime - s.utime + e.stime - s.stime
     end
 
+    def time_s
+      t = time.truncate
+      h = t / 3600
+      t = t % 3600
+      m = t / 60
+      t = t % 60
+      t += round(time - time.prec_i, 2)
+      
+      if (h > 0 && m > 0)
+        "#{h}h #{m}m #{t}s"
+      elsif (m > 0)
+        "#{m}m #{t}s"
+      else
+        "#{t}sec"
+      end
+    end
+
     def round(n, d)
       (n * 10 ** d).round / 10.0 ** d
     end
 
     def size_s
       if (@size > 1024 * 1024)
-       (round(@size / (1024 * 1024.0), 2)).to_s + " MB"
+        round(@size / (1024 * 1024.0), 2).to_s + "MB"
       elsif (@size > 1024)
-        (round(@size / 1024.0, 2)).to_s + " KB"
+        round(@size / 1024.0, 2).to_s + "KB"
       else
-        @size.to_s + " Byte"
+        @size.to_s + "Byte"
       end
     end
 
     def print(stdout)
-      stdout.puts "RESULT == #{@start_dir} (#{time} sec, #{size_s})"
+      stdout.puts "RESULT == #{@start_dir} (#{time_s}, #{size_s})"
       stdout.puts "files : #{@search_count} search in #{@count} files"
       stdout.puts "match : #{@match_file} files, #{match_count} match"
     end
