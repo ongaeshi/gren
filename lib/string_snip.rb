b0f9e68926cfa8d83161d8f77c7b8f6567154a3d
ongaeshi <tuto0621@.(none)>
2010-08-15 15:11:32 +0900

    単純な省略

--- lib/string_snip.rb
+++ lib/string_snip.rb
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 
 class StringSnip
-  def self.snip(str, ranges, size = 256, delimiter = '<<snip>>', pri = nil)
-    return str
+  def initialize(size = 256, delimiter = '<<snip>>', pri = nil)
+    @size = size
+    @delimiter = delimiter
+    @pri = pri
+  end
+
+  def snip(str, ranges)
+    @str = str
+    @ranges = ranges
+
+    # no snip
+    return @str if (@str.size <= @size)
+
+    # snip
+    results = []
+    @ranges.each {|r| results << @str[r] }
+    return results.join(@delimiter)
+  end
+
+  def substr(index)
+    @str[@ranges[index]]
   end
 end
 
--- test/test_string_snip.rb
+++ test/test_string_snip.rb
@@ -8,8 +8,12 @@ class TestStringSnip < Test::Unit::TestCase
   def test_string_snip
     str = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789|123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789|12345678901234567890123456789012345678901234567890123456"
 
-    snip_str = StringSnip::snip(str,
-                                [0..7, -8..-1])
-    assert_equal(str, snip_str)
+    snipper = StringSnip.new
+    snip_str = snipper.snip(str, [0..7, -8..-1])
+    assert_equal(snip_str, str)
+
+    snipper = StringSnip.new(128)
+    snip_str = snipper.snip(str, [0..7, -8..-1])
+    assert_equal(snip_str, "12345678<<snip>>90123456")
   end
 end
