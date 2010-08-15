c938012739f4ecc6c99c193a9427b35f61662da4
ongaeshi <ongaeshi@example.com>
2010-08-15 23:36:36 +0900

    v0.1.4

--- lib/gren.rb
+++ lib/gren.rb
@@ -2,5 +2,5 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Gren
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
