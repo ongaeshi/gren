1cb7e268f73d700379af66c9382074b7103eb6d9
ongaeshi <tuto0621@.(none)>
2010-08-15 13:48:22 +0900

    先頭、中間、終端で分離するように

--- lib/gren/util.rb
+++ lib/gren/util.rb
@@ -3,13 +3,13 @@
 module Gren
   class Util
     MAX_LINE_SIZE = 256
+    MARGIN_SIZE = 8
+    DELIMITER = '<<snip>>'
 
     def self.snip(str)
-      if (str.size > MAX_LINE_SIZE)
-        return str[0, MAX_LINE_SIZE]
-      else
-        return str
-      end
+      return str if (str.size < MAX_LINE_SIZE)
+
+      return str[0..7] + DELIMITER + str[64..128] + DELIMITER + str[-8..-1]
     end
   end
 end
