220aa0adb5c0c1af8ef85aa89bdfee5c8d5eb9c4
ongaeshi <tuto0621@.(none)>
2010-08-15 14:44:17 +0900

    抜粋を行ってくれるライブラリを先に作ることに

--- lib/gren/util.rb
+++ lib/gren/util.rb
@@ -6,10 +6,22 @@ module Gren
     MARGIN_SIZE = 8
     DELIMITER = '<<snip>>'
 
-    def self.snip(str)
-      return str if (str.size < MAX_LINE_SIZE)
+    def self.snip(str, match_datas)
+      return str if (str.size <= MAX_LINE_SIZE)
 
+#      p match_datas[0]
+#      p str.split(match_datas[0][0])
+#      p match_datas[0].pre_match, match_datas[0].post_match
+#      p match_datas[0].begin(0), match_datas[0].end(0)
       return str[0..7] + DELIMITER + str[64..128] + DELIMITER + str[-8..-1]
+
+#      match_datas.each do |m|
+#        line = line.split(m[0]).join("<42>#{m[0]}</42>"))
+#      end
+
+#      return str[0, MAX_LINE_SIZE]
     end
   end
 end
+
+
