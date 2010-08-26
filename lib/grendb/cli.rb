45fa622cdef4e625cb6adab1b332afcde53bea65
ongaeshi <ongaeshi@example.com>
2010-08-26 23:45:30 +0900

    コマンドラインを整理中

--- lib/grendb/cli.rb
+++ lib/grendb/cli.rb
@@ -1,42 +1,38 @@
+# -*- coding: utf-8 -*-
 require 'optparse'
 
 module Grendb
   class CLI
     def self.execute(stdout, arguments=[])
 
-      # NOTE: the option -p/--path= is given as an example, and should be replaced in your application.
+      opt = OptionParser.new <<EOF
+#{File.basename($0)} COMMAND [ARGS]
+--- subcommand ---
+        new                          Create new database.
+        add                          Add files.
+        update                       Update database.
 
-      options = {
-        :path     => '~'
+--- option -------
+EOF
+      opt.on('--db [DATABASE]', 'Database file path. (default is ".gren/default.db")') { |v| puts "--db : " + v.inspect }
+
+      subparsers = Hash.new {|h,k|
+        $stderr.puts "no such subcommand: #{k}"
+        exit 1
       }
-      mandatory_options = %w(  )
+      
+      subparsers['new'] = OptionParser.new
+      subparsers['new'].on('-i V') {|v| puts "-i :" + v.inspect}
+      
+      subparsers['add'] = OptionParser.new
+      subparsers['add'].on('--abc') {|v| puts "-i :" + v.inspect}
 
-      parser = OptionParser.new do |opts|
-        opts.banner = <<-BANNER.gsub(/^          /,'')
-          This application is wonderful because...
+      subparsers['update'] = OptionParser.new
 
-          Usage: #{File.basename($0)} [options]
-
-          Options are:
-        BANNER
-        opts.separator ""
-        opts.on("-p", "--path PATH", String,
-                "This is a sample message.",
-                "For multiple lines, add more strings.",
-                "Default: ~") { |arg| options[:path] = arg }
-        opts.on("-h", "--help",
-                "Show this help message.") { stdout.puts opts; exit }
-        opts.parse!(arguments)
-
-        if mandatory_options && mandatory_options.find { |option| options[option.to_sym].nil? }
-          stdout.puts opts; exit
-        end
-      end
-
-      path = options[:path]
-
-      # do stuff
-      stdout.puts "To update this executable, look in lib/grendb/cli.rb"
+      opt.order!(arguments)
+      subcommand = arguments.shift 
+      subparsers[subcommand].parse!(arguments) unless arguments.empty?
+      
     end
   end
 end
