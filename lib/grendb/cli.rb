a00a96748045bc37e3d538e1179e671344c21399
ongaeshi <ongaeshi@example.com>
2010-09-12 05:02:26 +0900

    grenを、grendbにコピー

--- lib/grendb/cli.rb
+++ lib/grendb/cli.rb
@@ -1,11 +1,65 @@
 # -*- coding: utf-8 -*-
 require 'optparse'
+require File.join(File.dirname(__FILE__), '../gren/findgrep')
 
 module Grendb
   class CLI
     def self.execute(stdout, arguments=[])
-      opt = OptionParser.new "#{File.basename($0)} INPUT_YAML1 [INPUT_YAML2 ...]"
-      stdout.puts opt.help
+      # オプション
+      option = Gren::FindGrep::DEFAULT_OPTION
+
+      # オプション解析
+      opt = OptionParser.new("#{File.basename($0)} [option] pattern")
+      opt.on('--sub PATTERN', 'Keyword is not included.') {|v| option.keywordsSub << v}
+      opt.on('--or PATTERN', 'Either of keyword is contained.') {|v| option.keywordsOr << v}
+      opt.on('-d DIR', '--directory DIR', 'Start directory. (deafult:".")') {|v| option.directory = v}
+      opt.on('--depth DEPTH', 'Limit search depth. ') {|v| option.depth = v.to_i}
+      opt.on('--this', '"--depth 0"') {|v| option.depth = 0}
+      opt.on('-i', '--ignore', 'Ignore case.') {|v| option.ignoreCase = true}
+      opt.on('-s', '--silent', 'Silent. Display match line only.') {|v| option.isSilent = true}
+      opt.on('--debug', 'Debug display.') {|v| option.debugMode = true}
+      opt.on('-c', '--color', 'Color highlight.') {|v| option.colorHighlight = true}
+      opt.on('-f REGEXP', '--file-regexp REGEXP', 'Search file regexp. (Enable multiple call)') {|v| option.filePatterns << v}
+      opt.on('--if REGEXP', '--ignore-file REGEXP', 'Ignore file pattern. (Enable multiple call)') {|v| option.ignoreFiles << v}
+      opt.on('--id REGEXP', '--ignore-dir REGEXP', 'Ignore dir pattern. (Enable multiple call)') {|v| option.ignoreDirs << v}
+      opt.on('-e ENCODE', '--encode ENCODE', 'Specify encode(none, auto, jis, sjis, euc, ascii, utf8, utf16). Default is "auto"') {|v| setupEncodeOption(option, v) }
+      opt.on('--no-snip', 'There being a long line, it does not snip.') {|v| option.noSnip = true }
+      opt.on('--db [GREN_DB_FILE]', 'Search from the grendb database.') {|v| option.dbFile = v }
+      opt.parse!(arguments)
+
+      # 検索オブジェクトの生成
+      if (arguments.size > 0 || option.keywordsOr.size > 0)
+        findGrep = Gren::FindGrep.new(arguments, option)
+        findGrep.searchAndPrint(stdout)
+      else
+        stdout.print opt.help
+      end
+      
+      # エンコードオプションの設定
+      def self.setupEncodeOption(option, encode)
+        case encode.downcase
+        when 'none'
+          option.kcode = Kconv::NOCONV
+        when 'auto'
+          option.kcode = Platform.get_shell_kcode
+        when 'jis'
+          option.kcode = Kconv::JIS
+        when 'sjis'
+          option.kcode = Kconv::SJIS
+        when 'euc'
+          option.kcode = Kconv::EUC
+        when 'ascii'
+          option.kcode = Kconv::ASCII
+        when 'utf8'
+          option.kcode = Kconv::UTF8
+        when 'utf16'
+          option.kcode = Kconv::UTF16
+        else
+          puts "Invalid encode."
+          puts "  none, auto, jis, sjis, euc, ascii, utf8, utf16"
+          exit(-1)
+        end
+      end
     end
   end
 end
