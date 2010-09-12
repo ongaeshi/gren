a202f41df12882f42a939afc2d81b6d8d5ad9654
ongaeshi <ongaeshi@example.com>
2010-09-13 01:28:36 +0900

    mkgrendbの-vオプションバグ
    -vが動作していなかった

--- lib/mkgrendb/cli.rb
+++ lib/mkgrendb/cli.rb
@@ -6,6 +6,7 @@ module Mkgrendb
   class CLI
     def self.execute(stdout, arguments=[])
       opt = OptionParser.new "#{File.basename($0)} INPUT_YAML1 [INPUT_YAML2 ...]"
+      opt.parse!(arguments)
 
       if (arguments.size >= 1)
         # @todo 複数個引数に対応
