7194753a3cbada768995e6c0b1e91d487f9bdf67
ongaeshi <ongaeshi@example.com>
2010-09-14 00:39:54 +0900

    --ddb, --defaultオプションを追加 (GRENDB_DEFAULT_DBで指定されているデータベースを更新)

--- lib/mkgrendb/cli.rb
+++ lib/mkgrendb/cli.rb
@@ -5,11 +5,16 @@ require File.join(File.dirname(__FILE__), 'mkgrendb')
 module Mkgrendb
   class CLI
     def self.execute(stdout, arguments=[])
+      input_yamls = []
+
       opt = OptionParser.new "#{File.basename($0)} INPUT_YAML1 [INPUT_YAML2 ...]"
+      opt.on('--ddb', "--default-db", "Create or Update default DB. (set ENV['GRENDB_DEFAULT_DB'])") {|v| input_yamls << ENV['GRENDB_DEFAULT_DB']}
       opt.parse!(arguments)
 
-      if (arguments.size >= 1)
-        arguments.each do |input_yaml|
+      input_yamls.concat arguments
+
+      if (input_yamls.size >= 1)
+        input_yamls.each do |input_yaml|
           obj = Mkgrendb.new(input_yaml)
           obj.update
           stdout.puts
