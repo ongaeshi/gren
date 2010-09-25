3e51093536a9fa8c41760ccd7c216f25fd749829
ongaeshi <ongaeshi@example.com>
2010-09-26 03:29:51 +0900

    v0.2.3 リリース
    * mkgrendb  * ymal入力時にパスを展開していないバグを修正  * mkgrendbの最後に表示する基本情報を整理    * 入力ファイル    * 出力ファイル    * かかった時間    * ファイル数    * 更新ファイル数  * データベースの生成オプションを整理    * パトリシア木 .. テーブルの記録方式、ハッシュに比べて遅くなるが、前方一致検索可能に    * 正規化 .. 各語彙を正規化して登録(大文字、小文字を区別しない)    * with_position .. 転置索引に出現位置情報を合わせて格納

---
+++ lib/common/display_util.rb
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+module Gren
+  module DisplayUtil
+    def time_s(time)
+      t = time.truncate
+      h = t / 3600
+      t = t % 3600
+      m = t / 60
+      t = t % 60
+      t += round(time - time.prec_i, 2)
+      
+      if (h > 0 && m > 0)
+        "#{h}h #{m}m #{t}s"
+      elsif (m > 0)
+        "#{m}m #{t}s"
+      else
+        "#{t}sec"
+      end
+    end
+    module_function :time_s
+
+    def round(n, d)
+      (n * 10 ** d).round / 10.0 ** d
+    end
+    module_function :round
+
+    def size_s(size)
+      tb = 1024 ** 4
+      gb = 1024 ** 3
+      mb = 1024 ** 2
+      kb = 1024
+
+      if (size >= tb)
+        round(size / tb.prec_f, 2).to_s + "TB"
+      elsif (size >= gb)
+        round(size / gb.prec_f, 2).to_s + "GB"
+      elsif (size >= mb)
+        round(size / mb.prec_f, 2).to_s + "MB"
+      elsif (size >= kb)
+        round(size / kb.prec_f, 2).to_s + "KB"
+      else
+        size.to_s + "Byte"
+      end
+    end
+    module_function :size_s
+  end
+end
